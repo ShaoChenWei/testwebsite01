9242f12e4bf9f92f0bb28dd3de1905096431f367
ShaoChenWei <80598149+ShaoChenWei@users.noreply.github.com>
2021-04-14 14:54:36 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// JavaScript 
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+   console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
